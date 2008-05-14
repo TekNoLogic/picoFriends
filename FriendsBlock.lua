695f5497825c7114b165b7b1e374c39e3414cafa
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-05-13 21:37:19 -0600

    New tooltip anchor function

--- FriendsBlock.lua
+++ FriendsBlock.lua
@@ -135,7 +135,7 @@ end
 local function GetTipAnchor(frame)
 	local x,y = frame:GetCenter()
 	if not x or not y then return "TOPLEFT", "BOTTOMLEFT" end
-	local hhalf = (x > UIParent:GetWidth()/2) and "RIGHT" or "LEFT"
+	local hhalf = (x > UIParent:GetWidth()*2/3) and "RIGHT" or (x < UIParent:GetWidth()/3) and "LEFT" or ""
 	local vhalf = (y > UIParent:GetHeight()/2) and "TOP" or "BOTTOM"
 	return vhalf..hhalf, frame, (vhalf == "TOP" and "BOTTOM" or "TOP")..hhalf
 end
