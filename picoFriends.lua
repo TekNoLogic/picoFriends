bb65de6bfbfc77c0e5c113a3e61f0038c412ce83
Tekkub <tekkub@gmail.com>
2008-11-17 23:08:27 -0700

    That crap's not needed anymore

--- picoFriends.lua
+++ picoFriends.lua
@@ -109,15 +109,6 @@ end
 --      Tooltip!      --
 ------------------------
 
-local function GetTipAnchor(frame)
-	local x,y = frame:GetCenter()
-	if not x or not y then return "TOPLEFT", "BOTTOMLEFT" end
-	local hhalf = (x > UIParent:GetWidth()*2/3) and "RIGHT" or (x < UIParent:GetWidth()/3) and "LEFT" or ""
-	local vhalf = (y > UIParent:GetHeight()/2) and "TOP" or "BOTTOM"
-	return vhalf..hhalf, frame, (vhalf == "TOP" and "BOTTOM" or "TOP")..hhalf
-end
-
-
 local tip = LibStub("tektip-1.0").new(4, "LEFT", "LEFT", "LEFT", "RIGHT")
 function dataobj.OnLeave() tip:Hide() end
 function dataobj.OnEnter(self)
