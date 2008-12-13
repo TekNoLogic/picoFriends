7928b29419fd6e474fdc414b26fb3bb90d83781f
Tekkub <tekkub@gmail.com>
2008-12-13 12:42:42 -0700

    Retarded server keeps padding my notes with spaces

--- picoFriends.lua
+++ picoFriends.lua
@@ -126,7 +126,7 @@ function dataobj.OnEnter(self)
 			local levelr, levelg, levelb = 0, 1, 0
 			if data.level < (mylevel - 5) then levelr, levelg, levelb = .6, .6, .6
 			elseif data.level > (mylevel + 5) then levelr, levelg, levelb = 1, 0, 0 end
-			tip:AddMultiLine(data.level, string.format("|cff%s%s|r%s%s", colors[data.class:gsub(" ", ""):upper()] or "ffffff", name, data.status == "" and "" or " ", data.status), data.note, data.area,
+			tip:AddMultiLine(data.level, string.format("|cff%s%s|r%s%s", colors[data.class:gsub(" ", ""):upper()] or "ffffff", name, data.status == "" and "" or " ", data.status), string.trim(data.note or ""), data.area,
 				levelr,levelg,levelb, nil,nil,nil, 1,0,1, 1,1,1)
 		end
 	end
